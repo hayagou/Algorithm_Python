3360429403527822fb0190463210ab42375efa90
HJ <de0978@gmail.com>
2021-07-03 18:29:09 +0900

    reforamt the file

--- basic/04.AverageAlgorithm.py
+++ basic/04.AverageAlgorithm.py
@@ -1,21 +1,21 @@
-#[?] n명의 점수 중에서 80점 이상 95점 이하인 점수의 평균
+# [?] n명의 점수 중에서 80점 이상 95점 이하인 점수의 평균
 
 # 평균 알고리즘(AverageAlgorithm) : 주어진 범위에 주어진 조건에 해당하는 자료들의 평균
 # 평균 알고리즘의 특징 : SUM 과 COUNT 알고리즘의 조합 
-#[1] Input : n명의 성적
-data = [ 90, 65, 78, 50, 95 ]
-sum = 0 # 합계 변수
-count = 0 # 개수 변수
-N = len(data) # 의사코드(슈도코드)
-#[2] Process : AVG = SUM / COUNT
+# [1] Input : n명의 성적
+data = [90, 65, 78, 50, 95]
+sum = 0  # 합계 변수
+count = 0  # 개수 변수
+N = len(data)  # 의사코드(슈도코드)
+# [2] Process : AVG = SUM / COUNT
 
-for i in range(0, N): # 주어진 범위
-    if data[i] >= 80 and data[i] <= 95: # 주어진 조건
-        sum += data[i] # SUM
-        count += 1 # COUNT
+for i in range(0, N):  # 주어진 범위
+    if data[i] >= 80 and data[i] <= 95:  # 주어진 조건
+        sum += data[i]  # SUM
+        count += 1  # COUNT
 
 avg = 0.0
-if sum != 0 and count != 0 :
-    avg = sum/ count # AVERAGE
-#[3] Output
+if sum != 0 and count != 0:
+    avg = sum / count  # AVERAGE
+# [3] Output
 print(f"80점 이상 95점 이하인 자료의 평균 : {avg:.2f}")
--- basic/06.MinAlgorithm.py
+++ basic/06.MinAlgorithm.py
@@ -1,5 +1,6 @@
-#[?] 주어진 데이터 중에서 가장 작은 [짝수] 값
+# [?] 주어진 데이터 중에서 가장 작은 [짝수] 값
 import sys
+
 
 # 최솟값 알고리즘(min Algorithm): (주어진 범위 + 주어진 조건)의 자료들의 가장 작은 값
 def main():
@@ -12,10 +13,11 @@ def main():
 
     # [2] Process : MAX
     for i in range(0, N):  # 주어진 범위
-        if numbers[i] < min and numbers[i] % 2 ==0:  # 더 큰 데이터가 있다면
+        if numbers[i] < min and numbers[i] % 2 == 0:  # 더 큰 데이터가 있다면
             min = numbers[i]  # MIN:  더 작은 값으로 할당
     # [3] Output
     print(f'짝수 최솟값: {min}')
+
 
 if __name__ == '__main__':
     main()
